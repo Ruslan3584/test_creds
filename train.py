3bf8609202b14e01df55cda5134cfd92ff1d8c18
ruslan khomenko <ruslankhomenko@pop-os.localdomain>
2021-10-19 12:46:03 +0300

    add TB logs

--- train.py
+++ train.py
@@ -1,8 +1,7 @@
 import numpy as np
+import datetime
 from keras.models import Sequential
 from keras.layers import Dense
-
-
 
 my_data = np.genfromtxt('anchors.csv', delimiter=',')
 
@@ -10,16 +9,23 @@ my_data = np.genfromtxt('anchors.csv', delimiter=',')
 X = my_data[:,:-1]
 y = my_data[:,-1]
 
-
 # define the keras model
 model = Sequential()
 model.add(Dense(8, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 # compile the keras model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-# fit the keras model on the dataset
-model.fit(X, y, epochs=10, steps_per_epoch=4, batch_size=2)
 
+log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+
+# fit the keras model on the dataset
+model.fit(X, y,
+          epochs=10, 
+          steps_per_epoch=4, 
+          batch_size=2, 
+          callbacks=[tensorboard_callback])
 
 
 _, accuracy = model.evaluate(X, y)
